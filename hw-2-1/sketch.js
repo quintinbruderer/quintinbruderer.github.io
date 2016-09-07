d2416a36c0a95a2a6b65ba1c2dd00d44d2368d02
Quinn Bruderer <quintinbruderer@gmail.com>
2016-09-07 11:24:16 -0600

    added start of homework as test

---
+++ hw-2-1/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(1000,2000);
+  background(color(0,180, 200));
+}
+
+function draw() {
+    strokeWeight (1);
+    ellipse(mouseX-70, mouseY, 55, 55);
+    ellipse(mouseX+70, mouseY, 55, 55);
+    triangle(mouseX,mouseY+25,mouseX-20,mouseY+45,mouseX+20,mouseY+45)
+    strokeWeight (4);
+    line(mouseX-80, mouseY+70, mouseX+80, mouseY+70);
+   curve(mouseX-80,mouseY+70,mouseX-40,mouseY+120,mouseX+40,mouseY+120,mouseX+80,mouseY+70)
+  }
